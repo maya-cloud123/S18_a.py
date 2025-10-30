b51498d2ea02b659c8236d943c6322f5f8863b0d
maya-cloud123 <a01802685@tec.mx>
2025-10-30 13:31:45 -0600

    Update and rename README.md to S18_A01802685.py

---
+++ S18_A01802685.py
@@ -0,0 +1,53 @@
+pares impares
+numeros=[22,24,38,40,48]
+pares=[]
+impares=[]
+for n in numeros:
+  if n % 2 == 0: 
+    pares.append(n)
+  else:
+    impares.append(n)
+
+
+numeros sin repetir 
+usuario=[]
+mientras verdadero 
+nombre= entrada("nombre (fin parasalir):").mas bajo()
+si nombre=="´´
+romper
+si nombre en usuario:
+imprimir("ese nimbre ya existe.")
+demas:
+usuario. añadir(nombre) 
+imprimir("usuarioa finales:",usuarios)
+
+temp=[14,16,18,20,22,24]
+medios de comunicacion= suma(temp)/prestado(temp)
+
+imprimir("temp:",mediosde comunicaccion) 
+para i en temperatura 
+si i medios de comunicaccion:
+ imprimir(i,"arribade la media")
+Elif i medios de comunicaccion:
+imprimir(i,"igual a la media")
+
+dinero delaware dos personas
+p1+[100,50,200]
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
