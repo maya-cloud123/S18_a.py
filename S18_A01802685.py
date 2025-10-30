7d17260cb6dd92d395a14025b34f3f002fa08f55
maya-cloud123 <a01802685@tec.mx>
2025-10-30 15:45:11 -0600

    Update S18_A01802685.py
    Fix variable names and improve user input handling.

--- S18_A01802685.py
+++ S18_A01802685.py
@@ -22,6 +22,50 @@ for temp in temperaturas:
 
 
 
+
+nombres = [“Maya”, “rey”, “mauri”, “nico”, “Juan”]
+notas = [88, 70, 35, 80, 60]
+
+promclase = sum(notas) / len(notas)
+print(“Promedio grupo:”, prom_clase)
+
+reprobados = []
+for i in range(len(nombres)):
+    if notas[i] < 70:
+        reprobados.append(nombres[i])
+
+print(“Alumnos reprobados:”, reprobados)
+
+aprobados = len(nombres) - len(reprobados)
+porcentaje = (aprobados * 100) / len(nombres)
+print(“Aprobados:”, porcentaje, “%”)
+
+
+numeros = [8, 3, 15, 2, 10, 7]
+print(“Número mayor:”, max(num))
+print(“Número menor:”, min(num))
+print(“Ordenados de menor a mayor:”, sorted(num))
+
+
+usuario = [“maya”, “pato”, “regina”]
+
+nuevo = input(“Pon un nombre“)
+
+while nuevo in users:
+    print(“ ese ya esta”)
+    nuevo = input(“Escribe un nombre diferente: “)
+
+users.(nuevo)
+print(“Usuario agregado:”, nuevo)
+print(“Usuarios ahora:”, nombre)
+
+
+
+
+
+
+
+
          
 
 
